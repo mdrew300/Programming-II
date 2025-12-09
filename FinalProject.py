96bbc1e4950b051b5c9577bd50a88a7a416a480d
mattd <mattdrew300@gmail.com>
2025-12-08 23:05:34 -0500

    Add FinalProject.py

--- FinalProject.py
+++ FinalProject.py
@@ -37,21 +37,23 @@ clean_sm(df)                 # and test your function to make sure it works as e
 
 # Create a new dataframe called "ss"
 ss = pd.DataFrame({'sm_li': clean_sm(s['web1h']), #target column called sm_li which should be a binary variable ( that takes the value of 1 if it is 1 and 0 otherwise (use clean_sm to create this) which indicates whether or not the individual uses LinkedIn
-       'income': np.where(s['income']<=9,         
+       'income': np.where(s['income']<=9,         #income (ordered numeric from 1 to 9, above 9 considered missing)     
                           s['income'], 
                           np.nan),
-       'education': np.where(s['educ2']<=8, 
+       'education': np.where(s['educ2']<=8,       # education (ordered numeric from 1 to 8, above 8 considered missing)
                              s['educ2'], 
                              np.nan),
-       'parent': clean_sm(s['par']),
-       'married': clean_sm(s['marital']),
-       'female': clean_sm(s['gender']),
-       'age': np.where(s['age'] <= 98, 
+       'parent': clean_sm(s['par']),              #parent (binary)
+       'married': clean_sm(s['marital']),         #married (binary)
+       'female': clean_sm(s['gender']),           #female (binary)
+       'age': np.where(s['age'] <= 98,            #age (numeric, above 98 considered missing)
                        s['age'], 
                        np.nan)}) 
 
 # Drop any missing values. 
 ss.dropna(inplace=True)
+
+# Perform exploratory analysis to examine how the features are related to the target.
 
 #ss.describe()
 
@@ -59,28 +61,55 @@ ss.dropna(inplace=True)
 
 ss['target_label'] = ss['sm_li'].map({0: "Not a LinkedIn User", 1: "LinkedIn User"})
 
+# Create a target vector (y) and feature set (X)
 y = 'sm_li'
 x = ['income', 'education', 'parent', 'married', 'female', 'age']
 
+# Split the data into training and test sets. Hold out 20% of the data for testing.
 train_data, test_data = train_test_split(
     ss,
     test_size=0.20,
     random_state=210,
     )
 
+# Explain what each new object contains and how it is used in machine learning
+
+#   After splitting the dataframe 'ss', the dataframe 'test_data' contains a subset of 20% of the data from ss that will be utilized to compare 
+# the predictions from the model and determine how well the model performs. The dataframe 'train_data' contains the remaining 80% of the data from ss  
+# that will be used for training the model, meaning that the variables will be observed and a mathematical relationship developed. More precisely,
+# because sm_li was assigned to y that will be the value we are predicting for (whether an individual is a LinkedIn user). The rest of the variables, 
+# assigned to x, will be used as the variables to determine the probability that an individual is a LinkedIn user. 
+
 #train_data.dtypes
 
-#Regression Model
+# Instantiate a logistic regression model and set class_weight to balanced. Fit the model with the training data.
+
+# Regression Model
 z = LogisticRegression(class_weight="balanced", random_state=210)
 model = z.fit(train_data[x], train_data[y])
 
+# Evaluate the model using the testing data. What is the model accuracy for the model? Use the model to make predictions and then 
 y_pred = model.predict(test_data[x])
+
+# generate a confusion matrix from the model
+# Create the confusion matrix as a dataframe and add informative column names and index names that indicate what each quadrant represents
 
 cm = pd.DataFrame(confusion_matrix(test_data[y], y_pred),
             columns=["Predicted negative", "Predicted positive"],
             index=["Actual negative","Actual positive"])
 
 #cm
+
+# Interpret the confusion matrix and explain what each number means.
+
+#   The confusion matrix displays the performance of the logistics regression model by calculating the number of positive and negative observations 
+# (LinkedIn users or non-user) and the number of predicted positive and negative observations. The top left intersection (Predicted negative and 
+# Actual negative) shows that the model correctly predicted 113 individuals as not being LinkedIn users, or a true negative. The top right intersection (Predicted positive
+# and Actual negative) shows that the model incorrectly predicted 51 individuals as being LinkedIn users when they were not, or a false positive. The bottom left intersection
+# (Predicted negative and Actual positive) shows that the model incorrectly predicted 22 individuals as not being LinkedIn users when they actually were, or a false negative.
+# The bottom right intersection (Predicted positive and Actual positive) shows that the model correctly predicted 66 individuals as being LinkedIn users, or a true positive.
+
+# Aside from accuracy, there are three other metrics used to evaluate model performance: precision, recall, and F1 score. Use the results in the confusion matrix to calculate each of these metrics by hand.
 
 precision = round(float(cm.at["Actual positive", "Predicted positive"] / cm["Predicted positive"].values.sum()),4)
 recall = round(float(cm.at["Actual positive", "Predicted positive"] / cm.loc["Actual positive"].values.sum()),4)
@@ -92,15 +121,24 @@ comp = pd.DataFrame([precision, recall, f1],
 
 #comp
 
+# Discuss each metric and give an actual example of when it might be the preferred metric of evaluation.
+
+#   The precision of the model states that 56.4% of the predicted LinkedIn users are actually LinkedIn users; the recall states that the model accurately predicts 75% of LinkedIn users; the F-1 Score provides a 
+# metric that evaluates both and is especially useful when classes are imbalanced in the data (Geeks for Geeks, 2025). Precision is more important when the risk of when the risk of a false positive is greater than 
+# the risk of a false negative, and recall is more important in the inverse scenario. 
+
+# After calculating the metrics by hand, create a classification_report using sklearn and check to ensure your metrics match those of the classification_report.
+
 print(classification_report(test_data[y], y_pred))
 
+# Use the model to make predictions. How does the probability change if another person is 82 years old, but otherwise the same?
 new_data = pd.DataFrame({
-    "income": [8, 8],
-    "education": [6, 6],
-    "parent": [0, 0],
-    "married": [1, 1],
-    "female": [1, 1],
-    "age": [42, 82]
+    "income": [8, 8],            # high income (e.g. income=8)
+    "education": [6, 6],         # high level of education (e.g. 7)
+    "parent": [0, 0],            # non-parent
+    "married": [1, 1],           # married
+    "female": [0, 0],            # female
+    "age": [42, 82]              # 42 years old
 })
 
 #new_data
@@ -170,8 +208,8 @@ with tab1:
         'Marital Status:',
         ss['Married_Label'].unique())
 
-    ss['Gender_Label'] = ss['female'].map({0: "Male",  
-                                        1: "Female"})
+    ss['Gender_Label'] = ss['female'].map({0: "Female",  
+                                        1: "Male"})
 
     optionGender = st.selectbox(
         'Gender:',
@@ -258,3 +296,11 @@ with tab3:
 
     st.pyplot(fig2)
     plt.close(fig2) 
+
+
+
+# Citations
+
+# “F1 Score in Machine Learning.” GeeksforGeeks, July 23, 2025. https://www.geeksforgeeks.org/machine-learning/f1-score-in-machine-learning/. 
+
+# “Precision and Recall in Machine Learning.” GeeksforGeeks, August 2, 2025. https://www.geeksforgeeks.org/machine-learning/precision-and-recall-in-machine-learning/. 
